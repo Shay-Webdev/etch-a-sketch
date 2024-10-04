4a641928dbba417ba381c98a36335ea22d3f484c
shay-Webdev <181019353+Shay-Webdev@users.noreply.github.com>
2024-10-04 13:08:46 +0530

    Add hovering to grids to draw
    Added hovering function using mouseover to change color
    while passing over a grid leaving a trail. This allows to draw on
    container grid.
    
    Usd javascript addEventListener to add mouseover method to grids

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 const container = document.querySelector("#container");
 
 for (let i = 0; i<256; i++){
-    let grid = document.createElement("div");
-    grid.classList.add("grid");
-    grid.style.width = `calc(100%/16)`;
-    grid.style.height = `calc(100%/16)`;
-    grid.style.margin = "0px";
-    grid.style.padding = "0px";
-    grid.style.backgroundColor = "grey";
-    grid.style.border = "1px solid black";
-    grid.style.boxSizing = "border-box";
-    grid.style.flex = "1 0 auto";
-    container.appendChild(grid);
+    const gridDiv = document.createElement("div");
+    gridDiv.classList.add("gridDiv");
+    container.appendChild(gridDiv);
 
 }
+
+const gridDivs = document.querySelectorAll('.gridDiv');
+
+gridDivs.forEach((gridDiv) => {
+    gridDiv.addEventListener("mouseover", () =>{
+        gridDiv.style.backgroundColor = "red";
+    });
+
+});
+
+
